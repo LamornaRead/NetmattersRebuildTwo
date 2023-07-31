a7adc80f586ee41ef25d841617fbad6b75ecc815
LamornaRead <130292971+LamornaRead@users.noreply.github.com>
2023-07-31 09:18:18 +0100

    page content

--- contact.php
+++ contact.php
@@ -4,9 +4,45 @@ include("inc/header.php");
 
 ?>
 
-<div>
-    <h2>Our offices</h2>
-</div>
+
+        <div>
+            <h3><strong>Home</strong> / Our Offices</h3>   
+        <div>
+
+        <div>
+            <h2>Our offices</h2>
+        </div>
+
+
+
+    <!-- office locations -->
+
+    <div>
+
+    </div>
+
+    <!-- contact form -->
+
+    <div>
+        <form action="">
+
+            <label for="name">Your Name <i class="fa-solid fa-asterisk fa-2xs" style="color: #c60500;"></i></label>
+            <input id="contact-name" class="" name="contact-name" type="text" value="">
+
+            <label for="company">Company Name</label>
+            <input id="contact-company" class="" name="contact-company" type="text" value="">
+            
+            <label for="email">Your Email <i class="fa-solid fa-asterisk fa-2xs" style="color: #c60500;"></i></label>
+            <input id="contact-email" class="" name="contact-email" type="text" value="">
+
+            <label for="number">Your Telephone Number <i class="fa-solid fa-asterisk fa-2xs" style="color: #c60500;"></i></label>
+            <input id="contact-name" class="" name="contact-number" type="text" value="">
+
+            <label for="company">Message <i class="fa-solid fa-asterisk fa-2xs" style="color: #c60500;"></i></label>
+            <textarea name="contact-message" id="contact-message" cols="30" rows="10"></textarea>
+        </form>
+    </div>
+
 
 <?php
 
